ca6d0c4b5e23a9284431e22ca81b966e8ae11b0f
Gabriel Manzotti <gabriel.manzotti@hotmail.com>
2022-01-17 19:40:25 -0300

    Commit inicial

---
+++ js code/script.js
@@ -0,0 +1,29 @@
+/* let nombre = prompt("ingrese su nombre");
+let añoNacimiento = prompt("Ingrese su año de nacimiento");
+
+let resultadoAñoNacimiento = parseFloat(añoNacimiento);
+const YEAR = 2022;
+
+let edad =  YEAR - resultadoAñoNacimiento;
+
+let saludo = `Hola ${nombre}, tenés ${edad} años`;
+
+/* alert(saludo); */
+
+/* let nombre = prompt("Ingrese su nombre");
+let apellido = prompt("Ingrese su apellido");
+ */
+
+
+
+let primerNumero = prompt("ingrese un precio");
+
+let segundoNumero = prompt ("Ingresa otro precio")
+
+let primerNumeroParse = parseFloat(primerNumero);
+let segundoNumeroParse = parseFloat (segundoNumero);
+
+if (primerNumeroParse >= 50 || segundoNumeroParse >= 50 ) {
+alert("uno de los dos números es 50");
+}
+
