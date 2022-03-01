ca198670870eafe46e30ae6046bbf76b2eaedc0e
Gabriel Manzotti <gabriel.manzotti@hotmail.com>
2022-03-01 17:43:15 -0300

    checkpoint antes de tarjeta v1

--- js code/reserva.js
+++ js code/reserva.js
@@ -76,6 +76,19 @@ class FormularioReserva {
 
 const datosFormulario = new FormularioReserva({});
 
+//se crea el objeto del pago de tarjeta
+class Formulariopago {
+    constructor(inputTC) {
+        this.numeroTarjeta = inputTC.numeroTarjeta;
+        this.nombreTarjeta = inputTC.nombreTarjeta;
+        
+    }
+}
+
+const datosFormularioTarjeta = new Formulariopago({});
+
+
+
 //boton elegir el plan Beca
 const botonPlanBeca = document.getElementById("botonPlanBeca")
 botonPlanBeca.addEventListener("click", (e) => {
@@ -108,7 +121,7 @@ const altura = document.getElementById("altura");
 const departamento = document.getElementById("departamento");
 
 const calle = document.getElementById("calle");
-const formulario = document.getElementById("formulario");
+const formulario1 = document.getElementById("formulario");
 
 
 // const eligeCurso = (arrayListadoCursoFiltrado) => {
@@ -125,8 +138,8 @@ const validar = function (campo) {
 };
 
 
-formulario.addEventListener("submit", (e) => {
-    e.preventDefault();
+formulario1.addEventListener("submit", (e) => {
+   
 
     validarEmail();
 
@@ -160,3 +173,146 @@ const validarEmail = () => {
         emailConfirma.classList.add("inputError");
     }
 }
+
+
+// --------------Tarjeta de crédito---------------------------
+
+const tarjeta = document.querySelector('#tarjeta'),
+	  btnAbrirFormulario = document.querySelector('#btn-abrir-formulario'),
+	  formulario = document.querySelector('#formulario-tarjeta'),
+	  numeroTarjeta = document.querySelector('#tarjeta .numero'),
+	  nombreTarjeta = document.querySelector('#tarjeta .nombre'),
+	  logoMarca = document.querySelector('#logo-marca'),
+	  firma = document.querySelector('#tarjeta .firma p'),
+	  mesExpiracion = document.querySelector('#tarjeta .mes'),
+	  yearExpiracion = document.querySelector('#tarjeta .year');
+	  ccv = document.querySelector('#tarjeta .ccv');
+      nombreTC = document.getElementById("inputNombre")
+      numeroTC = document.getElementById("inputNumero")
+      
+
+// * Volteamos la tarjeta para mostrar el frente.
+const mostrarFrente = () => {
+	if(tarjeta.classList.contains('active')){
+		tarjeta.classList.remove('active');
+	}
+}
+
+// * Rotacion de la tarjeta
+tarjeta.addEventListener('click', () => {
+	tarjeta.classList.toggle('active');
+});
+
+// * Boton de abrir formulario
+btnAbrirFormulario.addEventListener('click', () => {
+	btnAbrirFormulario.classList.toggle('active');
+	formulario.classList.toggle('active');
+});
+
+// * Select del mes generado dinamicamente.
+for(let i = 1; i <= 12; i++){
+	let opcion = document.createElement('option');
+	opcion.value = i;
+	opcion.innerText = i;
+	formulario.selectMes.appendChild(opcion);
+}
+
+// * Select del año generado dinamicamente.
+const yearActual = new Date().getFullYear();
+for(let i = yearActual; i <= yearActual + 8; i++){
+	let opcion = document.createElement('option');
+	opcion.value = i;
+	opcion.innerText = i;
+	formulario.selectYear.appendChild(opcion);
+}
+
+// * Input numero de tarjeta
+formulario.inputNumero.addEventListener('keyup', (e) => {
+	let valorInput = e.target.value;
+
+	formulario.inputNumero.value = valorInput
+	// Eliminamos espacios en blanco
+	.replace(/\s/g, '')
+	// Eliminar las letras
+	.replace(/\D/g, '')
+	// Ponemos espacio cada cuatro numeros
+	.replace(/([0-9]{4})/g, '$1 ')
+	// Elimina el ultimo espaciado
+	.trim();
+
+	numeroTarjeta.textContent = valorInput;
+
+	if(valorInput == ''){
+		numeroTarjeta.textContent = '#### #### #### ####';
+
+		logoMarca.innerHTML = '';
+	}
+
+	if(valorInput[0] == 4){
+		logoMarca.innerHTML = '';
+		const imagen = document.createElement('img');
+		imagen.src = "../Imagenes/img/logos/visa.png";
+		logoMarca.appendChild(imagen);
+	} else if(valorInput[0] == 5){
+		logoMarca.innerHTML = '';
+		const imagen = document.createElement('img');
+		imagen.src = '../Imagenes/img/logos/mastercard.png';
+		logoMarca.appendChild(imagen);
+	}
+
+	// Volteamos la tarjeta para que el usuario vea el frente.
+	mostrarFrente();
+});
+
+// * Input nombre de tarjeta
+formulario.inputNombre.addEventListener('keyup', (e) => {
+	let valorInput = e.target.value;
+
+	formulario.inputNombre.value = valorInput.replace(/[0-9]/g, '');
+	nombreTarjeta.textContent = valorInput;
+	firma.textContent = valorInput;
+
+	if(valorInput == ''){
+		nombreTarjeta.textContent = 'Jhon Doe';
+	}
+
+	mostrarFrente();
+});
+
+// * Select mes
+formulario.selectMes.addEventListener('change', (e) => {
+	mesExpiracion.textContent = e.target.value;
+	mostrarFrente();
+});
+
+// * Select Año
+formulario.selectYear.addEventListener('change', (e) => {
+	yearExpiracion.textContent = e.target.value.slice(2);
+	mostrarFrente();
+});
+
+// * CCV
+formulario.inputCCV.addEventListener('keyup', () => {
+	if(!tarjeta.classList.contains('active')){
+		tarjeta.classList.toggle('active');
+	}
+
+	formulario.inputCCV.value = formulario.inputCCV.value
+	// Eliminar los espacios
+	.replace(/\s/g, '')
+	// Eliminar las letras
+	.replace(/\D/g, '');
+
+	ccv.textContent = formulario.inputCCV.value;
+});
+
+const enviarPago = document.getElementById("enviarPago")
+
+enviarPago.addEventListener("click", (e) => {
+    e.preventDefault();
+    datosFormularioTarjeta.nombreTarjeta = nombreTC.value;
+    datosFormularioTarjeta.numeroTarjeta = numeroTC.value;
+    console.log(datosFormularioTarjeta)
+   
+})
+
